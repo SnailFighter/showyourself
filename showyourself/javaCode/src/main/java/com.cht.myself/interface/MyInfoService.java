d0f650daab61886ad401b15e9ba04279d573c7bd
snail <chaohongtao@gzhruizhi.com>
2017-06-20 20:55:23 +0800

    删除不用的文件

--- showyourself/javaCode/src/main/java/com.cht.myself/interface/MyInfoService.java
+++
@@ -1,7 +0,0 @@
-public interface MyInfoService {
-
-    public MyInfo query(long id);
-    public boolean insert(MyInfo info);
-    public boolean delete(long id);
-    public boolean modify(MyInfo info);
-}
